e73a29be996dc5f647a96438c1845fc8ef5d65e4
ctnswb <xclai.07@gmail.com>
2017-06-20 20:56:59 -0700

    app functions

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,45 @@
 // YOUR CODE HERE:
+var app = {
+
+  'init': function() {
+    $(document).ready()
+
+    return true;
+  },
+
+  'send': function(message) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function(data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+    return true;
+  },
+
+  'fetch': function() {
+    $.get(undefined);
+  },
+
+  'clearMessages': function() {
+    $('#chats').empty();
+  },
+
+  'renderMessage': function(message) {
+    $('#chats').append('<p>' + message.text + '</p>');
+  },
+
+  'renderRoom': function(roomName) {
+    $('#roomSelect').append('<p>' + roomName + '</p>'); //add room name
+  }
+
+
+};
